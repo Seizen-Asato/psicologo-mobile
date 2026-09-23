9f6279419eb55bb7d4df04f73c9e886c1507c886
t1629 <walterburique@gmail.com>
2026-09-23 08:04:51 -0300

    Add configuration files and update dependencies for project setup

--- SPY_Clinic_Mobile/services/baseUrl.js
+++ SPY_Clinic_Mobile/services/baseUrl.js
@@ -1,17 +1,26 @@
 import * as Device from "expo-device";
 import { Platform } from "react-native";
 
-let BASE_URL;
+const API_URL_DEVICE = process.env.EXPO_PUBLIC_API_URL_DEVICE;
+const API_URL_EMULATOR = process.env.EXPO_PUBLIC_API_URL_EMULATOR;
+const API_URL_WEB = process.env.EXPO_PUBLIC_API_URL_WEB;
+
+let baseUrl;
 
 if (Platform.OS === "android" && !Device.isDevice) {
-  // Emulador Android
-  BASE_URL = "http://10.0.2.2:5008/api";
+  baseUrl = API_URL_EMULATOR;
 } else if (Platform.OS === "web") {
-  // Web
-  BASE_URL = "http://localhost:5008/api";
+  baseUrl = API_URL_WEB;
 } else {
-  // Dispositivo físico
-  BASE_URL = "http://192.168.100.36:5008/api";
+  baseUrl = API_URL_DEVICE;
 }
+
+if (!baseUrl) {
+  throw new Error(
+    "Falta configurar la URL de la API. Revisa las variables EXPO_PUBLIC_API_URL_* en el archivo .env."
+  );
+}
+
+const BASE_URL = baseUrl;
 
 export { BASE_URL };
